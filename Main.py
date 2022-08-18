c78cf673b544a218eb3453ac599daaac0573177c
Ido Reshef <idoreshef@Idos-MacBook-Pro-2.local>
2022-08-18 09:47:00 +0300

    added a new file Main.py and ignore pycache

---
+++ Main.py
@@ -0,0 +1,5 @@
+import passwordgen
+
+passw = passwordgen.passwordg(int(input("How long do you want your password to be? ")))
+print("this is your newn password - ", passw)
+    
--- passwordgen.py
+++ passwordgen.py
@@ -1,13 +1,11 @@
 # making a password generator
 
 import random
+
+
 def passwordg(lenpass):
     chars = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()?"
     password = ""
     for i in range(lenpass):
         password += random.choice(chars)
     return password
-
-def main():
-    passw = passwordg(int(input("How long do you want your password to be? ")))
-    print("this is your newn password - ", passw)
